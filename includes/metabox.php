b76a188a0672db443f944ba56e88c0ac386dbfa4
vabrashev <36197901+vabrashev@users.noreply.github.com>
2019-06-27 15:11:39 +0300

    Changing the Insert button value

--- includes/metabox.php
+++ includes/metabox.php
@@ -58,7 +58,7 @@ function easy_image_gallery_metabox() {
                             <span class="name">Gallery</span>
                             <a href="#" class="dx-eig-gallery-add-images button" data-count="{{row-count-placeholder}}"><?php _e( 'Add images to the gallery', 'easy-image-gallery' ); ?></a>
                             <span class="eig-remove"><img src="<?php echo EASY_IMAGE_GALLERY_URL . 'includes/fonts/close.png'; ?>"></span>
-                            <a href="#" class="button button-primary button-small dx-eig-insert-shortcode">Insert</a>
+                            <a href="#" class="button button-primary button-small dx-eig-insert-shortcode">Insert this shortcode in the content</a>
                             <input type="text" class="dx-eig-shortcode" name="image_gallery[{{row-count-placeholder}}][SHORTCODE]" value="" hidden>
                             <input type="text" class="dx-eig-shortcode-show" readonly="" value="">
                             <div class="link-image-to-l">
@@ -131,7 +131,7 @@ function easy_image_gallery_metabox() {
                                     <span class="name">Gallery</span>
                                     <a href="#" class="dx-eig-gallery-add-images button" data-count="<?php echo $gallery_count;?>"><?php _e( 'Add images to the gallery', 'easy-image-gallery' ); ?></a>
                                     <span class="eig-remove"><img src="<?php echo EASY_IMAGE_GALLERY_URL . 'includes/fonts/close.png'; ?>"></span>
-                                    <a href="#" class="button button-primary button-small dx-eig-insert-shortcode">Insert</a>
+                                    <a href="#" class="button button-primary button-small dx-eig-insert-shortcode">Insert this shortcode in the content</a>
                                     <input type="text" class="dx-eig-shortcode" name="image_gallery[<?php echo $gallery_count;?>][SHORTCODE]" value="<?php echo $gallery['SHORTCODE'];?>" hidden>
                                     <input type="text" class="dx-eig-shortcode-show" readonly="" value='[easy_image_gallery gallery="<?php echo $gallery['SHORTCODE'];?>"]'>
                                     <div class="link-image-to-l">
