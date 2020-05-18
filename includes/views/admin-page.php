1103bdb4e94f11770b1986e1e66451ffa7490543
ivanviyachki <il.viyachki@gmail.com>
2020-05-18 17:40:20 +0300

    Add: added sidebar in admin page
    https://app.asana.com/0/1132076591483009/1155624685092489

--- includes/views/admin-page.php
+++ includes/views/admin-page.php
@@ -28,55 +28,81 @@ $settings = (array) get_option( 'easy-image-gallery', $defaults );
 $lightbox = esc_attr( $settings['lightbox'] );
 ?>
 <div class="wrap">
-	<form action='' method='post'>
-		<h1><?php echo __( 'Easy Image Gallery Settings', 'easy-image-gallery' ); ?></h1>
-		<table class="form-table" role="presentation">
-			<tbody>
-				<tr>
-					<th scope="row"><?php echo __( 'Lightbox', 'easy-image-gallery' ); ?></th>
-					<td>
-						<select name="easy-image-gallery[lightbox]">
-							<?php foreach ( easy_image_gallery_lightbox() as $key => $label ) : ?>
-								<option value="<?php echo $key; ?>" <?php selected( $lightbox, $key ); ?>><?php echo $label; ?></option>
-							<?php endforeach; ?>
-						</select>
-					</td>
-				</tr>
-				<tr>
-					<th scope="row"><?php echo __( 'Post Types', 'easy-image-gallery' ); ?></th>
-					<td>
-						<?php
-						foreach ( easy_image_gallery_get_post_types() as $key => $label ) :
+	<div class="left-info">
+		<form action='' method='post'>
+			<h1><?php echo __( 'Easy Image Gallery Settings', 'easy-image-gallery' ); ?></h1>
+			<table class="form-table" role="presentation">
+				<tbody>
+					<tr>
+						<th scope="row"><?php echo __( 'Lightbox', 'easy-image-gallery' ); ?></th>
+						<td>
+							<select name="easy-image-gallery[lightbox]">
+								<?php foreach ( easy_image_gallery_lightbox() as $key => $label ) : ?>
+									<option value="<?php echo $key; ?>" <?php selected( $lightbox, $key ); ?>><?php echo $label; ?></option>
+								<?php endforeach; ?>
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<th scope="row"><?php echo __( 'Post Types', 'easy-image-gallery' ); ?></th>
+						<td>
+							<?php
+							foreach ( easy_image_gallery_get_post_types() as $key => $label ) :
 
-								$post_types = isset( $settings['post_types'][ $key ] ) ? esc_attr( $settings['post_types'][ $key ] ) : '';
-							?>
-							<p>
-								<input type="checkbox" id="<?php echo $key; ?>" name="easy-image-gallery[post_types][<?php echo $key; ?>]" <?php checked( $post_types, 'on' ); ?>/><label for="<?php echo $key; ?>"> <?php echo $label; ?></label>
-							</p>
-						<?php endforeach; ?>
-					</td>
-				</tr>
-			</tbody>
-		</table>
-		<?php
-		$arr_grids_filter = apply_filters( 'add_more_grids_support', $arr_grid_views );
-		?>
-		<h2><?php echo __( 'Easy Image Gallery Grid Views', 'easy-image-gallery' ); ?></h2>
-		<table class="form-table" role="presentation">
-			<tbody>
-				<tr>
-					<th scope="row"><?php echo __( 'Grid View', 'easy-image-gallery' ); ?></th>
-					<td>
-						<select name="easy-image-gallery[grid_view]">
-							<?php foreach ( $arr_grids_filter as $key => $label ) : ?>
-								<option value="<?php echo $key; ?>" <?php selected( $settings['grid_view'], $key ); ?>><?php echo $label; ?></option>
+									$post_types = isset( $settings['post_types'][ $key ] ) ? esc_attr( $settings['post_types'][ $key ] ) : '';
+								?>
+								<p>
+									<input type="checkbox" id="<?php echo $key; ?>" name="easy-image-gallery[post_types][<?php echo $key; ?>]" <?php checked( $post_types, 'on' ); ?>/><label for="<?php echo $key; ?>"> <?php echo $label; ?></label>
+								</p>
 							<?php endforeach; ?>
-						</select>
-					</td>
-				</tr>
-			</tbody>
-		</table>
-		<?php wp_nonce_field( 'eig_admin_page_save', 'eig_admin_page' ); ?>
-		<?php submit_button(); ?>
-	</form>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<?php
+			$arr_grids_filter = apply_filters( 'add_more_grids_support', $arr_grid_views );
+			?>
+			<h2><?php echo __( 'Easy Image Gallery Grid Views', 'easy-image-gallery' ); ?></h2>
+			<table class="form-table" role="presentation">
+				<tbody>
+					<tr>
+						<th scope="row"><?php echo __( 'Grid View', 'easy-image-gallery' ); ?></th>
+						<td>
+							<select name="easy-image-gallery[grid_view]">
+								<?php foreach ( $arr_grids_filter as $key => $label ) : ?>
+									<option value="<?php echo $key; ?>" <?php selected( $settings['grid_view'], $key ); ?>><?php echo $label; ?></option>
+								<?php endforeach; ?>
+							</select>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<?php wp_nonce_field( 'eig_admin_page_save', 'eig_admin_page' ); ?>
+			<?php submit_button(); ?>
+		</form>
+	</div>
+
+	<div class="right-info">
+		<h3>Check out more of our premium plugins</h3>
+		<li><a target="_blank" href="https://bg.wordpress.org/plugins/dx-localhost/">DX localhost</a> Display a notice when working on a localhost site on staging environment, based on your settings and adjustments
+		Super helpful whenever you’re cloning a production website with virtual hosts using the same domain name, and are not sure which site are you editing. Visible both in the admin area, and the frontend to all site visitors.
+		</li>
+		<li><a target="_blank" href="https://wordpress.org/themes/masonry/">Masonry theme</a> Our Masonry theme is based on the popular and efficient grid library, suitable for creatives, photographers or bloggers.
+		</li>
+		<p>View more of our <a target="_blank" href="https://profiles.wordpress.org/devrix/#content-plugins">Free</a> and <a target="_blank" href="https://devrix.com/shop">Premium</a> plugins and themes.</p>
+		<p><hr/></p>
+
+		<h3>Are you looking for help?</h3>
+		<p>Don't worry, we got you covered:</p>
+		<li><a href="http://wordpress.org/support/plugin/dx-rss-feeds">Plugin support forum on WordPress</a></li>
+		<li><a href="https://devrix.com/contact">Contact Us</a></li>
+		<li><a href="https://devrix.com/articles">Browse our blog for tutorials</a></li>
+		<p><hr/></p>
+
+		<p>
+			<li><a href="https://wordpress.org/support/view/plugin-reviews/dx-rss-feeds?rate=5#postform">Give us &#9733;&#9733;&#9733;&#9733;&#9733; rating</a></li>
+			<li><a href="http://twitter.com/wpdevrix" target="_blank">Follow on Twitter</a></li>
+		</p>
+	</div>
+
 </div>
