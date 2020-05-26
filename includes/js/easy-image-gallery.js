b990ca659ba8b569129085566caa58bee696bbf7
ivanviyachki <il.viyachki@gmail.com>
2020-05-26 18:20:14 +0300

    Fix: added watermark for popup images for Luminous lightbox
    https://app.asana.com/0/1132076591483009/1155776700300425

--- includes/js/easy-image-gallery.js
+++ includes/js/easy-image-gallery.js
@@ -7,4 +7,14 @@ jQuery(function() {
         opacity: 0.7,
         margin: 12
     });
+
+    jQuery( ".eig-popup" ).click(function() {
+        jQuery('.lum-img, .fancybox-image, #fullResImage').watermark({
+            text: site_title,
+            textWidth: 100,
+            gravity: 'se',
+            opacity: 0.7,
+            margin: 20
+        });
+    });
 });
