823d0aec4258b0961d0de8c91ae4b0cc8a201784
stanimir-georgiev <stanimir.georgiev_99@abv.bg>
2020-11-06 18:44:51 +0200

    Style the watermark

--- includes/js/easy-image-gallery.js
+++ includes/js/easy-image-gallery.js
@@ -2,10 +2,11 @@ jQuery(function() {
     var site_title = (script_vars.site_title);
     jQuery('.attachment-thumbnail').watermark({
         text: site_title,
-        textWidth: 100,
+        textWidth: 125,
         gravity: 'se',
         opacity: 0.7,
-        margin: 12
+        margin: 12,
+        textSize: 16,
     });
 
     jQuery( ".eig-popup" ).click(function() {
@@ -14,7 +15,8 @@ jQuery(function() {
             textWidth: 100,
             gravity: 'se',
             opacity: 0.7,
-            margin: 20
+            margin: 20,
+            textSize: 16,
         });
     });
 });
