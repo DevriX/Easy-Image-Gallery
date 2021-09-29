1936c47de1869517393fd4d9c8b09acc95d47be1
Tatyana Asenova <tasenova@devrix.com>
2021-09-29 19:06:58 +0300

    Fixing a bug in one of the JS files. ASANA task: no particular task, part of the debugging process of the plugin

--- twenty-twentyone-style-specific.js
+++ twenty-twentyone-style-specific.js
@@ -2,7 +2,9 @@
 
 jQuery( document ).ready(function() {
 	console.log('hello')
-	document.querySelector('ul script').style.display='none';
+	if (document.querySelector('ul script')) {
+		document.querySelector('ul script').style.display='none';
+	}
     Array.from(document.querySelectorAll('div.easy-image-gallery img')).forEach(img => img.style = {})
 	
 });
