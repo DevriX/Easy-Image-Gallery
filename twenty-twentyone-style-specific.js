8d31c8167cd2a0e50bfc6f816c99a83b246befbe
Tatyana Asenova <tasenova@devrix.com>
2021-09-29 16:24:01 +0300

    Fixed script that got displayed on FE. ASANA: https://app.asana.com/0/529531421086025/1201084433649140/f

--- twenty-twentyone-style-specific.js
+++ twenty-twentyone-style-specific.js
@@ -1,6 +1,9 @@
 
+
 jQuery( document ).ready(function() {
 	console.log('hello')
+	document.querySelector('ul script').style.display='none';
     Array.from(document.querySelectorAll('div.easy-image-gallery img')).forEach(img => img.style = {})
+	
 });
 
