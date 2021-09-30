7c60c04db0686c437c5a9b19ba915f6a55eb82aa
Tatyana Asenova <tasenova@devrix.com>
2021-09-30 19:39:49 +0300

    Full functionality is present. To-do: A few more tests before getting it to QA. ASANA https://app.asana.com/0/529531421086025/1134345346057337/f

--- eig_like_btn_script.js
+++ eig_like_btn_script.js
@@ -6,6 +6,8 @@ jQuery( document ).ready(function() {
 			lgLikeBtn = document.querySelector('button.lg-like-btn')
 			tackleLgHoverLike(lgLikeBtn);
 			likeLgPhoto(lgLikeBtn)
+			let images = Array.from(document.querySelectorAll('.lg-thumb.lg-group img'))
+			images.forEach(img => img.addEventListener('click',()=> {document.querySelector('.lg-likes-span').innerHTML=''}))
 	
 		})
 	}
@@ -15,9 +17,12 @@ jQuery( document ).ready(function() {
 			lgLikeBtn = document.querySelector('button.lg-like-btn')
 			tackleLgHoverLike(lgLikeBtn);
 			likeLgPhoto(lgLikeBtn)
+			let images = Array.from(document.querySelectorAll('.lg-thumb.lg-group img'))
+			images.forEach(img => img.addEventListener('click',()=> {document.querySelector('.lg-likes-span').innerHTML=''}))
 		 }, 20);
 
 	}
+
 
  })
 
@@ -51,7 +56,18 @@ jQuery( document ).ready(function() {
 				url: ajax_object.ajax_url,
 				data: data,
 				success: function (response) {
-					console.log(response.data)
+					//console.log(response.data)
+					for (const key in response.data) {
+						if (key==isActivePhoto) {
+							console.log(response.data[key]['likes']);
+							let likesSpan = document.createElement('span');
+							likesSpan.classList.add("lg-likes-span")
+							likesSpan.style.cssText ="font-size: 21px;color: #999;line-height: 27px;position: absolute;right: 25px;text-align: center;top: -33px;width: 210px;z-index: 35;"
+							likesSpan.innerHTML=`${response.data[key]['likes']} likes`
+							element.before(likesSpan);
+						}
+						console.log(key);
+					}
 				},
 				error: function (response) {
 					console.log(response)
@@ -61,5 +77,6 @@ jQuery( document ).ready(function() {
 		}
 	 })
  }
+
 
 
