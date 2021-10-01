18de350ca48d26805677b74f271613aee74db178
Tatyana Asenova <tasenova@devrix.com>
2021-10-01 11:56:38 +0300

    Fixed a few php notices. ASANA https://app.asana.com/0/529531421086025/1134345346057337/f

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -565,13 +565,23 @@ function eig_tackle_lg_likes() {
 		update_option('eig_liked_images', $liked_images);
 	 }
 	else {
+		$is_present = false;
+		foreach ( $previously_liked as $key=>$value ) {
+			if ($key == $lg_liked_photo) {
+				$is_present=true;
+			}
+		}
+		if ($is_present==false) {
+			 $previously_liked[$lg_liked_photo] = ['likes' => 0];
+			 update_option('eig_liked_images', $previously_liked);
+		}
+		//wp_send_json_success($is_present);
+		// wp_send_json_success($previously_liked[$lg_liked_photo]['likes']);
 		$new_likes = $previously_liked[$lg_liked_photo]['likes'] + 1;
 		$previously_liked[$lg_liked_photo] = ['likes' => $new_likes];
 		update_option('eig_liked_images', $previously_liked);
 
-		}
+	}
 
-
-
-	wp_send_json_success($previously_liked);
+	wp_send_json_success( get_option('eig_liked_images') );
 }
