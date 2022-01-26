8caf43abfe83f2c5f55110d86acacf3ec87c79b5
Vladislav Abrashev <vabrashev@gmail.com>
2022-01-26 18:03:43 +0200

    Adding a menu icon for the EIG dashboard link

--- includes/admin-page.php
+++ includes/admin-page.php
@@ -13,7 +13,9 @@ function add_eig_plugin_page() {
 		__( 'Easy Image Gallery Settings', 'easy-image-gallery' ),
 		__( 'Easy Image Gallery', 'easy-image-gallery' ),
 		'manage_options',
-		EASY_IMAGE_GALLERY_DIR . 'includes/view/admin-page-view.php'
+		EASY_IMAGE_GALLERY_DIR . 'includes/view/admin-page-view.php',
+		'',
+		'dashicons-images-alt2',
 	);
 }
 add_action( 'admin_menu', 'add_eig_plugin_page' );
